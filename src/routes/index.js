62f3d2190bec0f400739af68d447150574d4386c
denev <denev723@gmail.com>
2024-12-27 02:56:38 +0900

    router, state, subscribe

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ import { createRouter } from "../core/denev";
 import Home from "./Home";
 import About from "./About";
 
-export default createRouter([
+const routes = [
   { path: "#/", component: Home },
   { path: "#/about", component: About },
-]);
+];
+
+export default createRouter(routes);
---
+++ src/store/message.js
@@ -0,0 +1,5 @@
+import { Store } from "../core/denev";
+
+export default new Store({
+  message: "Hello Fuck you",
+});
